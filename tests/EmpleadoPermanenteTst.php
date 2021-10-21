5e1e9ad6de6e77a8d75eadbbca659d2bced330b6
tomasguida <tomasguida2000@hotmail.com>
2021-10-21 15:36:18 -0300

    Se añade empleadopermanentetst

---
+++ tests/EmpleadoPermanenteTst.php
@@ -0,0 +1,25 @@
+<?php
+namespace App;
+require_once 'EmpleadoTest.php';
+
+//La clase debe heredar de la clase Empleado:
+class EmpleadoEventualTest extends EmpleadoTest
+{
+    public function testcomision()
+    {
+     $r = new \ app\EmpleadoPermanente(10);
+     $this -> assertEquals(10, $r ->calcularantiguedad());
+      return  $this -> calcularantiguedad()%;
+    }
+    public function testcalcularIngresoTotal($salario = 4500){
+    $r = new \ app\EmpleadoPermanente(10);
+    $this -> assertEquals(10, $r ->calcularantiguedad());
+    return $this->$salario + $this->$salario * $this->calcularAntiguedad() / 100;
+    }
+
+    public function testcalcularAntiguedad(fechaingreso = 2010/07/10) {
+        $r = $this->fechaIngreso->diff(new \DateTime()); 
+        return $r;
+    }
+   
+}
